acedfbb2262f17d227544fa5cffd4969384afeea
Aniket Tiwari <pt5602238@gmail.com>
2026-05-08 18:25:07 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -25,7 +25,7 @@ const cors = require('cors')
 app.use(cors({
   origin: [
     "http://localhost:5173",
-    "https://frontend-coding-roan.vercel.app/"   
+    "https://frontend-coding-roan.vercel.app"   
   ],
   credentials: true
 }));
